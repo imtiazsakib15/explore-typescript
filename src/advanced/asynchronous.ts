92f8244708fd005af4d9705a7d467b0990009227
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-05-06 12:58:06 +0600

    asynchronous functions in typescript

---
+++ src/advanced/asynchronous.ts
@@ -0,0 +1,40 @@
+{
+  // Asynchronous behavior of TypeScript
+
+  const getPromise = <T>(param: T): Promise<T> =>
+    new Promise((resolve, reject) => {
+      if (param) resolve(param);
+      else reject("Failed to load data!");
+    });
+
+  const str: string = "Something";
+  const user = { name: "Sakib", age: 24 };
+
+  interface User {
+    name: string;
+    age: number;
+  }
+
+  const getData = async <T>(param: T): Promise<void> => {
+    const data: T = await getPromise(param);
+    console.log({ data });
+  };
+  getData<string>(str);
+  getData<User>(user);
+
+  // Fetch post from server
+  interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }
+
+  const getPost = async (): Promise<Post> => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await res.json();
+    console.log(data);
+    return data;
+  };
+  getPost();
+}
