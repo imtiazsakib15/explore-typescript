c97cd5bd22af587854d5ddb37e034660b7954b22
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-05-07 18:17:25 +0600

    class in oop typescript

--- src/objectOriented/class.ts
+++ src/objectOriented/class.ts
@@ -1,6 +1,7 @@
 {
   // Class in Object Oriented Programming
-  class Student {
+  /* 
+    class Student {
     constructor(public name: string, public roll: number) {}
     intro() {
       console.log(`My name is ${this.name}. My roll is ${this.roll}.`);
@@ -8,6 +9,24 @@
   }
 
   const student1 = new Student("abcd", 1);
+
+  student1.intro();
+  */
+
+  class Student {
+    name: string;
+    roll: number;
+
+    constructor(name: string, roll: number) {
+      this.name = name;
+      this.roll = roll;
+    }
+    intro() {
+      console.log(`My name is ${this.name}. My roll is ${this.roll}.`);
+    }
+  }
+
+  const student1 = new Student("efgh", 2);
 
   student1.intro();
 
